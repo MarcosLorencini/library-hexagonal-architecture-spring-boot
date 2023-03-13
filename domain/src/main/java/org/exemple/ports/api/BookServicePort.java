d9cc8cb4d30ccad629e57e9fa263ef4a3d87c3df
MarcosLorencini <mlorencinif@gmail.com>
2023-03-13 16:35:58 -0300

    port api created

---
+++ domain/src/main/java/org/exemple/ports/api/BookServicePort.java
@@ -0,0 +1,16 @@
+package org.exemple.ports.api;
+
+import org.exemple.data.BookDto;
+
+import java.util.List;
+
+public interface BookServicePort {
+
+    BookDto addBook(BookDto bookDto);
+    void deleteBookId(Long id);
+    BookDto updateBook(BookDto bookDto);
+    List<BookDto> getBooks();
+    BookDto getBookById(Long bookId);
+
+
+}
