f377cd78bef95c9b150a675e5aa5968aef145b64
MarcosLorencini <mlorencinif@gmail.com>
2023-03-13 20:46:24 -0300

    mappers created

--- infrastructure/src/main/java/org/example/mappers/BookMapper.java
+++ infrastructure/src/main/java/org/example/mappers/BookMapper.java
@@ -1,10 +1,26 @@
 package org.example.mappers;
 
-import org.mapstruct.Mapper;
 
 //MapStruct is a code generator that greatly simplifies the implementation of mappings between
 // Java bean types based on a convention over configuration approach.
 
+import org.example.entity.Book;
+import org.exemple.data.BookDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
 @Mapper
 public interface BookMapper {
+
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    BookDto bookToBookDto(Book book);
+
+    Book bookDtoToBook(BookDto bookDto);
+
+    List<BookDto> bookListToBookDtoList(List<Book> bookList);
+
+    List<Book> bookDtoListToBookList(List<BookDto> bookDtoList);
 }
