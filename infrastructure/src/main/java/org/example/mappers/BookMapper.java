a5a360d6e9f73a0b4b29de8a39db20dbc681498d
MarcosLorencini <mlorencinif@gmail.com>
2023-03-13 20:41:25 -0300

    infrastructure created

---
+++ infrastructure/src/main/java/org/example/mappers/BookMapper.java
@@ -0,0 +1,10 @@
+package org.example.mappers;
+
+import org.mapstruct.Mapper;
+
+//MapStruct is a code generator that greatly simplifies the implementation of mappings between
+// Java bean types based on a convention over configuration approach.
+
+@Mapper
+public interface BookMapper {
+}
